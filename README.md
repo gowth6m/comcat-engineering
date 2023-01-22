a44b056120bd56e83dd51d9cf6095de2a01071cc
Gowthaman Ravindrathas <gowtham4n@gmail.com>
2023-01-22 18:28:12 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Great Comcat Engineering
+
+[View Website](https://greatcomcatengineering.com)
+
+E-commerce website for Great Comcat Engineering Ltd based in London, UK. This web site was built using Next.js, Framer Motion, Tailwind css and MongoDB for backend to store data.
+
+To get dependencies
+```
+npm install
+```
+
+To run website in developer mode
+```
+npm run dev
+```
+
+To build
+```
+npm run build
+```
---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Great Comcat Engineering</title>
+        <meta name="description" content="Website for Great Comcat Engineering" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/logo/logo.svg" />
+      </Head>
+      <main className={styles.main}>
+        <h1 className="text-3xl text-red-300">HELLO WORLD</h1>
+      </main>
+    </>
+  );
+}
