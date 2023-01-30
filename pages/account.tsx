7db1915734a456983a8f0dbf27a507ff0534a0c4
Gowthaman Ravindrathas <gowtham4n@gmail.com>
2023-01-30 02:14:06 +0000

    Update account.tsx

--- pages/account.tsx
+++ pages/account.tsx
@@ -1,12 +1,26 @@
 import Layout from "@/components/Layout";
 import { Auth } from "@/utils/Auth";
+import { useSession } from "next-auth/react";
+import router from "next/router";
+import Router from "next/router";
 import React from "react";
 
 export default function AccountScreen() {
+  const { data: session }: any = useSession();
+
+  if (!session?.user) {
+    router.push("/login");
+  }
+
   return (
     <Auth>
       <Layout title="My Account">
-        <div className="heading1">My Account</div>
+        <div className="md:mt-4 mt-2">
+          <div className="text-2xl text-black">
+            Welcome back
+            <span className="text-[var(--orange)]"> {session?.user.name}</span>
+          </div>
+        </div>
       </Layout>
       ;
     </Auth>
