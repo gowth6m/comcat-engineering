30c96b2841eae9b1e5d7c4ec0a156d1aa7022969
Gowthaman Ravindrathas <gowtham4n@gmail.com>
2023-01-30 23:53:34 +0000

    minor fix

--- components/IntroGallery.tsx
+++ components/IntroGallery.tsx
@@ -1,19 +1,32 @@
 /* eslint-disable @next/next/no-img-element */
+import useWindowDimensions from "@/utils/window";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import React from "react";
 
 export default function IntroGallery() {
+  const { width } = useWindowDimensions() ?? { width: 0 };
+
   return (
     <Splide
       aria-label="Intro Slider"
       className="w-[100vw] left-0 md:top-24 absolute z-0 m-0 p-0 top-12 bg-[var(--black)]"
-      options={{
-        rewind: true,
-        gap: "10rem",
-        height: "20rem",
-        arrows: false,
-        autoplay: true,
-      }}
+      options={
+        Number(width) > 768
+          ? {
+              rewind: true,
+              gap: "10rem",
+              height: "20rem",
+              arrows: false,
+              autoplay: true,
+            }
+          : {
+              rewind: true,
+              gap: "10rem",
+              height: "10rem",
+              arrows: false,
+              autoplay: true,
+            }
+      }
     >
       <SplideSlide className="object-cover w-full h-full">
         <img
@@ -25,6 +38,13 @@ export default function IntroGallery() {
       <SplideSlide className="object-cover w-full h-full">
         <img
           src={"/assets/galleryImg_1.svg"}
+          alt="Image 2"
+          className="object-cover w-full h-full scale-150 md:scale-100"
+        />
+      </SplideSlide>
+      <SplideSlide className="object-cover w-full h-full">
+        <img
+          src={"/assets/galleryImg_2.svg"}
           alt="Image 2"
           className="object-cover w-full h-full scale-150 md:scale-100"
         />
